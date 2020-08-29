6618408740f384c015bf58de30ad6230fbf7d087
Mahin <mahinyayaa@gmail.com>
2020-08-29 19:25:40 +0600

    axios, recharts, metarial ui discuss

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import News from './components/News/News';
+import Charts from './components/Charts/Charts';
+import axios from 'axios';
 
 function App() {
+  const [articles, setArticles] = useState([])
+  // useEffect(() => {
+  //   fetch('https://newsapi.org/v2/top-headlines?country=us&apiKey=76ee43e1e8634f4094a3bf04fc45b5a3')
+  //   .then(res => res.json())
+  //   .then(data => setArticles(data.articles))
+  // }, [])
+  useEffect( () => {
+    const url = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=76ee43e1e8634f4094a3bf04fc45b5a3';
+    axios(url)
+    .then(data => setArticles(data.data.articles))
+  }, [])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Headlines: {articles.length}</h2>
+      <Charts></Charts>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
